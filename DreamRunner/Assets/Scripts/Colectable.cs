1c16a259acc6c63640e322a1822c2480c93e78df
Raulloco <raul-luar@hotmail.com.br>
2023-01-12 02:08:22 -0300

    Coletavel,vida e mario hat

---
+++ DreamRunner/Assets/Scripts/Colectable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colectable : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Collider2D col;
+    public GameObject[] Colects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            //dissapear?
+            sr.enabled = false;
+            col.enabled = false;
+            //Call Colect Objects
+            for (int i = 0; i < Colects.Length; i++)
+            {
+                Colects[i].SetActive(!Colects[i].activeSelf);
+            }
+            //Completly dissapear
+            Destroy(gameObject, 1f);
+        }
+    }
+}
